20781540fb4523b880aadfe79fe655a08d80c753
Diego Gonzalo <72668810@certus.edu.pe>
2022-12-15 09:44:46 -0500

    controllers actualizados

--- src/main/java/com/example/backendh93p1/controller/PreguntaController.java
+++ src/main/java/com/example/backendh93p1/controller/PreguntaController.java
@@ -2,11 +2,14 @@ package com.example.backendh93p1.controller;
 
 import com.example.backendh93p1.entity.Examen;
 import com.example.backendh93p1.entity.Pregunta;
+import com.example.backendh93p1.services.ExamenService;
 import com.example.backendh93p1.services.PreguntaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/pregunta")
@@ -16,23 +19,73 @@ public class PreguntaController {
     @Autowired
     private PreguntaService preguntaService;
 
-    @GetMapping
-    public List<Pregunta> busquedapregunta (){
-        return (List<Pregunta>) preguntaService.obtenerPreguntas();
+    @Autowired
+    private ExamenService examenService;
+
+    @PostMapping("/agregar")
+    public ResponseEntity<Pregunta> guardarPregunta(@RequestBody Pregunta pregunta){
+        return ResponseEntity.ok(preguntaService.agregarPregunta(pregunta));
     }
 
-    @PostMapping
-    public void crearpregunta (@RequestBody Pregunta preguntaClient){
-        preguntaService.agregarPregunta(preguntaClient);
+    @PutMapping("/modificar")
+    public ResponseEntity<Pregunta> actualizarPregunta(@RequestBody Pregunta pregunta){
+        return ResponseEntity.ok(preguntaService.actualizarPregunta(pregunta));
     }
 
-    @PutMapping
-    public void actualizarPregunta (@RequestBody Pregunta preguntaClient){
-        preguntaService.actualizarPregunta(preguntaClient);
+    @GetMapping("/examen/{examenId}")
+    public ResponseEntity<?> listarPreguntasDelExamen(@PathVariable("examenId") Long examenId){
+        Examen examen = examenService.obtenerExamen(examenId);
+        Set<Pregunta> preguntas = examen.getPreguntas();
+
+        List examenes = new ArrayList(preguntas);
+        if(examenes.size() > Integer.parseInt(examen.getNumeroDePreguntas())){
+            examenes = examenes.subList(0,Integer.parseInt(examen.getNumeroDePreguntas() + 1));
+        }
+
+        Collections.shuffle(examenes);
+        return ResponseEntity.ok(examenes);
     }
 
-    @GetMapping (value = "{id}")
-    public Pregunta obtenerPreguntaId (@PathVariable ("idpregunta") Long idpregunta){
-        return preguntaService.obtenerPregunta(idpregunta);
+    @GetMapping("/obtener/{preguntaId}")
+    public Pregunta listarPreguntaPorId(@PathVariable("preguntaId") Long preguntaId){
+        return preguntaService.obtenerPregunta(preguntaId);
+    }
+
+    @DeleteMapping("/eliminar/{preguntaId}")
+    public void eliminarPregunta(@PathVariable("preguntaId") Long preguntaId){
+        preguntaService.eliminarPregunta(preguntaId);
+    }
+
+    @GetMapping("/examen/todos/{examenId}")
+    public ResponseEntity<?> listarPreguntaDelExamenComoAdministrador(@PathVariable("examenId") Long examenId){
+        Examen examen = new Examen();
+        examen.setExamenId(examenId);
+        Set<Pregunta> preguntas = preguntaService.obtenerPreguntasDelExamen(examen);
+        return ResponseEntity.ok(preguntas);
+    }
+
+    @PostMapping("/evaluar-examen")
+    public ResponseEntity<?> evaluarExamen(@RequestBody List<Pregunta> preguntas){
+        double puntosMaximos = 0;
+        Integer respuestasCorrectas = 0;
+        Integer intentos = 0;
+
+        for(Pregunta p : preguntas){
+            Pregunta pregunta = this.preguntaService.listarPregunta(p.getPreguntaId());
+            if(pregunta.getRespuesta().equals(p.getRespuestaDada())){
+                respuestasCorrectas ++;
+                double puntos = Double.parseDouble(preguntas.get(0).getExamen().getPuntosMaximos())/preguntas.size();
+                puntosMaximos += puntos;
+            }
+            if(p.getRespuestaDada() != null){
+                intentos ++;
+            }
+        }
+
+        Map<String,Object> respuestas = new HashMap<>();
+        respuestas.put("puntosMaximos",puntosMaximos);
+        respuestas.put("respuestasCorrectas",respuestasCorrectas);
+        respuestas.put("intentos",intentos);
+        return ResponseEntity.ok(respuestas);
     }
 }
